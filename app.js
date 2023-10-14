7c91a78e133d2c5412ec7d74fa4479fbc9d38dd6
Coccio2101 <coccio2101@gmail.com>
2023-10-14 18:09:14 +0200

    feat: use asynchronus function and add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash")
+const mongoose = require("mongoose")
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -15,45 +16,50 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// Get requests
-app.get("/", (req, res) => {
-    res.render("home", {content: homeStartingContent, posts: posts})
-})
-
-app.get("/about", (req, res) => {
-    res.render("about", {content: aboutContent})
-})
-
-app.get("/contact", (req, res) => {
-    res.render("contact", {content: contactContent})
-})
-
-app.get("/compose", (req, res) => {
-    res.render("compose")
-})
-
-app.get("/posts/:postTitle", (req, res) => {
-    posts.forEach((post) => {
-        if (_.lowerCase(req.params.postTitle) === _.lowerCase(post.title)) {
-            res.render("post", {postTitle: post.title, postBlog: post.content})
-        }
+async function main() {
+    // Get requests
+    app.get("/", (req, res) => {
+        res.render("home", {content: homeStartingContent, posts: posts})
     })
-})
 
-// Post requests
-app.post("/compose", (req, res) => {
-    const post = {
-        title: req.body.postTitle,
-        content: req.body.postBody
-    }
+    app.get("/about", (req, res) => {
+        res.render("about", {content: aboutContent})
+    })
 
-    posts.push(post)
-    res.redirect("/")
-})
+    app.get("/contact", (req, res) => {
+        res.render("contact", {content: contactContent})
+    })
+
+    app.get("/compose", (req, res) => {
+        res.render("compose")
+    })
+
+    app.get("/posts/:postTitle", (req, res) => {
+        posts.forEach((post) => {
+            if (_.lowerCase(req.params.postTitle) === _.lowerCase(post.title)) {
+                res.render("post", {postTitle: post.title, postBlog: post.content})
+            }
+        })
+    })
+
+    // Post requests
+    app.post("/compose", (req, res) => {
+        const post = {
+            title: req.body.postTitle,
+            content: req.body.postBody
+        }
+
+        posts.push(post)
+        res.redirect("/")
+    })
 
 
 
-// Server listen
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+    // Server listen
+    app.listen(3000, function() {
+    console.log("Server started on port 3000");
+    });
+}
+
+main()
+.catch((err) => console.log("Error occured: " + e))
